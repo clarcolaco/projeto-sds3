fc803c82766fdc85fe5a6b7c25b611703012691e
Clarissa Colaco <clarissa.colaco@hotmail.com>
2021-09-11 21:13:28 -0300

    Adicionando apexcharts

--- frontend/src/assets/componets/Footer/index.tsx
+++ frontend/src/assets/componets/Footer/index.tsx
@@ -3,7 +3,7 @@ const Footer = () => {
 
         <footer className="footer mt-auto py-3 bg-dark">
             <div className="container">
-                <p className="text-light">App desenvolvido por <a href="https://github.com/acenelio" target="_blank" rel="noreferrer">Nelio Alves</a></p>
+                <p className="text-light">App desenvolvido por <a href="https://github.com/clarcolaco" target="_blank" rel="noreferrer">Clarissa Colaço</a> @ 2021</p>
                 <p className="text-light"><small><strong>Semana Spring React</strong><br />
                     Evento promovido pela escola DevSuperior: <a href="https://instagram.com/devsuperior.ig" target="_blank" rel="noreferrer">@devsuperior.ig</a></small></p>
             </div>
