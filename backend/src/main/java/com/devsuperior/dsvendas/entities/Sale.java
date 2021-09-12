0ce4ea2960009e5774eeb69da0621b901d5601a1
Clarissa Colaco <clarissa.colaco@hotmail.com>
2021-09-11 22:02:56 -0300

    Mapping sale completed

--- backend/src/main/java/com/devsuperior/dsvendas/entities/Sale.java
+++ backend/src/main/java/com/devsuperior/dsvendas/entities/Sale.java
@@ -2,15 +2,29 @@ package com.devsuperior.dsvendas.entities;
 
 import java.time.LocalDate;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "tb_sales")
 public class Sale {
 
-	
+		@Id
+		@GeneratedValue(strategy = GenerationType.IDENTITY)
 		private Long id;
 		private Integer visited;
 		private Integer deals;
 		private Integer amount;
 		private LocalDate date;
 		
+		
+		@ManyToOne
+		@JoinColumn(name = "seller_id")
 		private Seller seller;
 		
 		public Sale() {
