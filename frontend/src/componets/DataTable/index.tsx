fcade2294606afce59ecafc46bba7cf31d758f7c
Clarissa Colaco <clarissa.colaco@hotmail.com>
2021-09-12 17:46:02 -0300

    Botões integrados com table

--- frontend/src/componets/DataTable/index.tsx
+++ frontend/src/componets/DataTable/index.tsx
@@ -1,10 +1,13 @@
 import axios from "axios";
+import Pagination from "componets/Pagination";
 import { useEffect, useState } from "react";
 import { SalePage } from "types/sale";
 import { formatLocalDate } from "utils/format";
 import { BASE_URL } from "utils/requests";
 
 const DataTable = () => {
+    
+    const [activePage, setActivePage] = useState(0);
     
     const [page, setPage] = useState<SalePage>(
     
@@ -19,14 +22,21 @@ const DataTable = () => {
     
     useEffect(() => {
     
-        axios.get(`${BASE_URL}/sales?page=0&size=20&sort=date,desc`)
+        axios.get(`${BASE_URL}/sales?page=${activePage}&size=20&sort=date,desc`)
         .then(response => {
             setPage(response.data);
         })
-    }, []); 
+    }, [activePage]); 
+    
+    const changePage = (index: number) =>{
+        setActivePage(index);
+    
+    }
     
     
     return(
+    <>
+        <Pagination page={page} onPageChange={changePage}/>
         <div className="table-responsive">
         <table className="table table-striped table-sm">
             <thead>
@@ -57,7 +67,9 @@ const DataTable = () => {
                    
             </tbody>
         </table>
-    </div>);
+    </div>
+    
+    </>);
 
 }
 
