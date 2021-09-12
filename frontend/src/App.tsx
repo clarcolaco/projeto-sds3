c5f5166aa716d68ebaabfa457f6a3873b99bb73a
Clarissa Colaco <clarissa.colaco@hotmail.com>
2021-09-11 21:33:58 -0300

    Frontend finalizado + components

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,12 +1,46 @@
+
+import BarChart from 'assets/componets/BarChart';
+import DataTable from 'assets/componets/DataTable';
+import DonutChart from 'assets/componets/DonutChart';
+import Footer from 'assets/componets/Footer';
 import NavBar from 'assets/componets/NavBar';
 import React from 'react';
 
 function App() {
   return (
-    <> 
-    
-    <NavBar />
-    
+    <>
+      <NavBar />
+      <div className="container"><h1 className="text-primary py-3">
+        Dashboard de Vendas
+      </h1>
+      <div className="row px-3">
+        <div className="col-sm-6">
+        <h5 className="text-center text-secondary">Taxa de Sucesso (%)</h5>
+        <BarChart/>
+        </div>
+        <div className="col-sm-6">
+        <h5 className="text-center text-secondary">Vendas</h5>
+        <DonutChart/>
+        </div>
+      </div>
+      
+      
+      
+      
+      <div className="py-3">
+      <h2 className="text-primary">
+      Todas as vendas
+      </h2>
+      
+      </div>
+      
+      
+      <DataTable/>
+      </div>
+      
+      
+      <Footer/>
+
     </>
   );
 }
