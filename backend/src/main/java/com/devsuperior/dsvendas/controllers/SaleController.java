ea02da65c3be4e3e7d9306ef39740b97c68250a5
Clarissa Colaco <clarissa.colaco@hotmail.com>
2021-09-12 13:05:36 -0300

    Grouped by Search

--- backend/src/main/java/com/devsuperior/dsvendas/controllers/SaleController.java
+++ backend/src/main/java/com/devsuperior/dsvendas/controllers/SaleController.java
@@ -10,6 +10,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.devsuperior.dsvendas.dto.SaleDTO;
+import com.devsuperior.dsvendas.dto.SaleSuccessDTO;
 import com.devsuperior.dsvendas.dto.SaleSumDTO;
 import com.devsuperior.dsvendas.service.SaleService;
 
@@ -32,6 +33,14 @@ public class SaleController {
 	@GetMapping(value ="/amount-by-seller")
 	public ResponseEntity<List<SaleSumDTO>> amountGroupedBySeller() {
 		List<SaleSumDTO> list = service.amountGroupedBySeller();
+		return ResponseEntity.ok(list);
+		
+	}
+	
+	
+	@GetMapping(value ="/success-by-seller")
+	public ResponseEntity<List<SaleSuccessDTO>> successGroupedBySeller() {
+		List<SaleSuccessDTO> list = service.successGroupedBySeller();
 		return ResponseEntity.ok(list);
 		
 	}
--- backend/src/main/java/com/devsuperior/dsvendas/service/SaleService.java
+++ backend/src/main/java/com/devsuperior/dsvendas/service/SaleService.java
@@ -6,7 +6,9 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
 import com.devsuperior.dsvendas.dto.SaleDTO;
+import com.devsuperior.dsvendas.dto.SaleSuccessDTO;
 import com.devsuperior.dsvendas.dto.SaleSumDTO;
 import com.devsuperior.dsvendas.entities.Sale;
 import com.devsuperior.dsvendas.repositories.SaleRepository;
@@ -31,6 +33,12 @@ public class SaleService {
 	@Transactional(readOnly = true)
 	public List<SaleSumDTO> amountGroupedBySeller(){
 		return repository.amountGroupedBySeller();
+		
+	}
+	
+	@Transactional(readOnly = true)
+	public List<SaleSuccessDTO> successGroupedBySeller(){
+		return repository.successGroupedBySeller();
 		
 	}
 	
